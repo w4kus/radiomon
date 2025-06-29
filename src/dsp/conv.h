b4be4d6943ad3b0c8127763c448e4619c5cb5272
Mark White <w4kus.jmw@gmail.com>
2025-06-29 14:21:50 -0400

    Optimization

--- src/dsp/conv.h
+++ src/dsp/conv.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 John Mark White -- US Amateur Radio License: W4KUS
+//
+// Licensed under the MIT License - see LICENSE file for details.
+
 #pragma once
 
 #include <vector>
@@ -16,20 +20,17 @@ public:
         if (len <= 0)
             return std::vector<T>(0);
 
-        size_t k = n - 1;
-        size_t max;
+        size_t k = n + 1;
         size_t min;
+
         std::vector<T> res(len);
 
         for (size_t i=0;i < (size_t)len;i++,k++)
         {
             res[i] = 0;
-            
-            min = (k >= (n - 1)) ? k - (n - 1) : 0;
-            max = (k < (m - 1)) ? k : (m - 1);
-
-            for (size_t j=min;j <= max;j++)
-                res[i] += b[j] * h[k - j];
+            min = k - n + 1;
+            for (size_t j=0;j < n; j++)
+                res[i] += b[min+j] * h[j];
         }
 
         return res;
