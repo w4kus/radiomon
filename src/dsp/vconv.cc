1953e6f2a911fd4959465350c30f9f9329ab7e70
Mark White <w4kus.jmw@gmail.com>
2025-06-29 14:27:49 -0400

    Changes:
    * Fix indexing.
    * Optimize the convolution loop.
    * Fix a miscalculated size.

--- src/dsp/vconv.cc
+++ src/dsp/vconv.cc
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 John Mark White -- US Amateur Radio License: W4KUS
+//
+// Licensed under the MIT License - see LICENSE file for details.
+
 #include <volk/volk.h>
 #include <cstring>
 #include <vconv.h>
@@ -9,25 +13,22 @@ std::vector<float> dsp::vconvolve(const float *b, const size_t m, const float *h
     if (len <= 0)
         return std::vector<float>(0);
 
-    size_t k = n - 1;
+    size_t k = n + 1;
     size_t min;
 
     std::vector<float> res(len);
 
-    float *aligned_b_buff = (float *)volk_malloc(n * sizeof(float), volk_get_alignment());
+    float *aligned_b_buff = (float *)volk_malloc(m * sizeof(float), volk_get_alignment());
     float *aligned_h_buff  = (float *)volk_malloc(n * sizeof(float), volk_get_alignment());
 
     std::memcpy(aligned_h_buff, h, n * sizeof(float));
+    std::memcpy(aligned_b_buff, b, m * sizeof(float));
 
     for (size_t i=0;i < (size_t)len;i++, k++)
     {
         res[i] = 0;
-
-        min = (k >= (n - 1)) ? k - (n - 1) : 0;
-
-        std::memcpy(aligned_b_buff, &b[min], n * sizeof(float));
-
-        volk_32f_x2_dot_prod_32f(&res[i], aligned_b_buff, aligned_h_buff, n);
+        min = k - n + 1;
+        volk_32f_x2_dot_prod_32f(&res[i], &aligned_b_buff[min], aligned_h_buff, n);
     }
 
     volk_free(aligned_b_buff);
@@ -43,25 +44,22 @@ std::vector<std::complex<float>> dsp::vconvolve(const std::complex<float> *b, co
     if (len <= 0)
         return std::vector<std::complex<float>>(0);
 
-    size_t k = n - 1;
+    size_t k = n + 1;
     size_t min;
 
     std::vector<std::complex<float>> res(len);
 
-    std::complex<float> *aligned_b_buff = (std::complex<float> *)volk_malloc(n * sizeof(std::complex<float>), volk_get_alignment());
-    float *aligned_h_buff  = (float *)volk_malloc(n * sizeof(float), volk_get_alignment());
+    std::complex<float> *aligned_b_buff = (std::complex<float> *)volk_malloc(m * sizeof(std::complex<float>), volk_get_alignment());
+    float *aligned_h_buff = (float *)volk_malloc(n * sizeof(float), volk_get_alignment());
 
     std::memcpy(aligned_h_buff, h, n * sizeof(float));
+    std::memcpy(aligned_b_buff, b, m * sizeof(std::complex<float>));
 
     for (size_t i=0;i < (size_t)len;i++, k++)
     {
         res[i] = 0;
-
-        min = (k >= (n - 1)) ? k - (n - 1) : 0;
-
-        std::memcpy(aligned_b_buff, &b[min], n * sizeof(std::complex<float>));
-
-        volk_32fc_32f_dot_prod_32fc(&res[i], aligned_b_buff, aligned_h_buff, n);
+        min = k - n + 1;
+        volk_32fc_32f_dot_prod_32fc(&res[i], &aligned_b_buff[min], aligned_h_buff, n);
     }
 
     volk_free(aligned_b_buff);
--- src/dsp/vconv.h
+++ src/dsp/vconv.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 John Mark White -- US Amateur Radio License: W4KUS
+//
+// Licensed under the MIT License - see LICENSE file for details.
+
 #pragma once
 
 #include <vector>
@@ -6,6 +10,7 @@
 namespace dsp {
 
 std::vector<float> vconvolve(const float *b, const size_t n, const float *h, const size_t m);
+
 std::vector<std::complex<float>> vconvolve(const std::complex<float> *b, const size_t n, const float *h, const size_t m);
 
 }
