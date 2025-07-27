67f53bad0bc77d84fb7d4a9fee9478649f52eedf
Mark White <w4kus.jmw@gmail.com>
2025-07-27 16:13:45 -0400

    Refactor Comments for Doxygen

--- src/dsp/ops/delay.h
+++ src/dsp/ops/delay.h
@@ -1,7 +1,17 @@
+// Copyright (c) 2025 John Mark White -- US Amateur Radio License: W4KUS
+//
+// Licensed under the MIT License - see LICENSE file for details.
+
 #pragma once
 
 namespace dsp
 {
+
+/*! \brief Delay block
+ *
+ * A small template to implement single sample delays.
+ *
+*/
 
 template<typename T>
 class delay
@@ -9,8 +19,15 @@ class delay
 public:
 
     delay() = delete;
+
+    //! Create an instance.
+    //! @param [in] initRet The value returned when given the first sample.
+    //! Default to zero.
     delay(const T initRet = 0) : z(initRet) {}
 
+    //! Insert a new sample into the delay block.
+    //! @param [in] sample The new sample.
+    //! @return The current sample in the delay block.
     T ins(const T sample)
     {
         auto ret = z;
@@ -18,6 +35,8 @@ public:
         return ret;
     }
 
+    //! Return the current sample in the delay block.
+    //! @return The current sample in the delay block.
     T get() { return z; }
 
 private:
