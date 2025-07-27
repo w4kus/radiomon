c4ab398dea4705d8049ae4b8204b038597d51120
Mark White <w4kus.jmw@gmail.com>
2025-07-27 15:59:35 -0400

    Add Print Utilities

--- src/utils/cmdline.h
+++ src/utils/cmdline.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <complex>
+#include <stdio.h>
 
 namespace util {
 
 char *getCmdOption(char ** begin, char ** end, const std::string& option);
 bool cmdOptionExists(char** begin, char** end, const std::string& option);
 
+void printComplex(FILE *f, std::vector<std::complex<float>> &vals);
+void printComplex(FILE *f, size_t num, std::complex<float> *vals);
+void printReal(FILE *f, std::vector<float>  &vals);
 }
