489a4672c026679988fe461bbb7976ae397fe8e9
Mark White <w4kus.jmw@gmail.com>
2025-08-10 12:50:33 -0400

    Changes:
    * Add printReal function.
    * Add const modifier to printComplex.
    * Fix an index bug in printComplex.

--- src/utils/cmdline.h
+++ src/utils/cmdline.h
@@ -11,6 +11,7 @@ char *getCmdOption(char ** begin, char ** end, const std::string& option);
 bool cmdOptionExists(char** begin, char** end, const std::string& option);
 
 void printComplex(FILE *f, std::vector<std::complex<float>> &vals);
-void printComplex(FILE *f, size_t num, std::complex<float> *vals);
+void printComplex(FILE *f, size_t num, const std::complex<float> *vals);
 void printReal(FILE *f, std::vector<float>  &vals);
+void printReal(FILE *f, size_t num, const float *vals);
 }
