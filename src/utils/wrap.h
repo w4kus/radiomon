ebec77204f60801c44e686862064c8c5e67ec3e2
Mark White <w4kus.jmw@gmail.com>
2025-10-08 21:13:25 -0400

    Inline the Volk Function Templates

--- src/utils/wrap.h
+++ src/utils/wrap.h
@@ -17,17 +17,16 @@ namespace util {
 template<typename T, typename U, typename V>
 void dot_prod(T *out, const U *in, const V *taps, unsigned int num_points)
 {
-    assert(1);
 }
 
 template<>
-void dot_prod(float *out, const float *in, const float *taps, unsigned int num_points)
+inline void dot_prod(float *out, const float *in, const float *taps, unsigned int num_points)
 {
     volk_32f_x2_dot_prod_32f(out, in, taps, num_points);
 }
 
 template<>
-void dot_prod(std::complex<float> *out, const std::complex<float> *in, const float *taps, unsigned int num_points)
+inline void dot_prod(std::complex<float> *out, const std::complex<float> *in, const float *taps, unsigned int num_points)
 {
     volk_32fc_32f_dot_prod_32fc(out, in, taps, num_points);
 }
