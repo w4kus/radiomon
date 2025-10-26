a08c84e0a990b95fb5c42789193443b8c83308d8
Mark White <w4kus.jmw@gmail.com>
2025-10-26 09:34:08 -0400

    Initial Rev of freq-est and Some Source File Additions / Subractions

---
+++ src/utils/freq-est.cc
@@ -0,0 +1,33 @@
+// Copyright (c) 2025 John Mark White -- US Amateur Radio License: W4KUS
+//
+// Licensed under the MIT License - see LICENSE file for details.
+
+#include "freq-est.h"
+#include "rm-math.h"
+
+using namespace util;
+
+float freq_est::estimate(const aligned_ptr<complex_f> &in)
+{
+    // delayed sample vector
+    auto v2 = make_aligned_ptr<complex_f>(in.size());
+
+    // result vector
+    auto v3 = make_aligned_ptr<complex_f>(in.size());
+
+    // Run the data through the delay block to make the second vector
+    for (size_t i=0;i < in.size();i++)
+        v2[i] = m_Delay << in[i];
+
+    // Do the math
+    rm_math::mult_conj(&v3[0], in.get(), v2.get(), in.size());
+
+    // Sum the result to average out the noise
+    complex_f sum = { 0.0f, 0.0f };
+    for (size_t i=0;i < in.size();i++)
+        sum += v3[i];
+
+    // Finally determine the phase difference which is the current
+    // frequency estimate.
+    return m_Scale * rm_math::atan2(sum);
+}
---
+++ src/utils/freq-est.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2025 John Mark White -- US Amateur Radio License: W4KUS
+//
+// Licensed under the MIT License - see LICENSE file for details.
+
+#pragma once
+
+#include "aligned-ptr.h"
+#include "delay.h"
+
+namespace util {
+
+/*! \brief Frequency Estimater
+ *
+ * Given a block of a complex signal, this will estimate the frequency of
+ * that signal within the given block. It uses the delay / multiply technique
+ * which relies on the phase difference between successive samples. This can
+ * be incorporated into a frequency lock loop (FLL) as the frequency error
+ * detector (FED) since it provides a quick estimate which can then be sent
+ * to, e.g., a PLL.
+ */
+
+class freq_est
+{
+public:
+    freq_est() = delete;
+
+    freq_est(const freq_est&) = delete;
+    freq_est& operator=(const freq_est&) = delete;
+
+    freq_est(freq_est&&) = delete;
+    freq_est& operator=(freq_est&&) = delete;
+
+    //! Create an instance of the estimater.
+    //! @param [in] scale   The value to use to scale the result. If set to 1.0 (no scaling),
+    //!                     the output is in units of radians / sample. If you want the
+    //!                     output to be in Hz, then use **Fs/(2*PI)** where **Fs**
+    //!                     is the sampling rate.
+    freq_est(float scale) : m_Scale { scale } { }
+
+    //! Estimate a block of samples.
+    //! @param [in] in      The block of samples with which to make an estimation.
+    //! @return The scaled estimate.
+    float estimate(const aligned_ptr<complex_f> &in);
+
+private:
+    float m_Scale;
+    delay<complex_f> m_Delay;
+};
+
+}
