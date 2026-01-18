1a29ae11f82f6f3c932674ec53e3de00c2d1bff8
Mark White <w4kus.jmw@gmail.com>
2026-01-18 17:49:11 -0500

    Switch to 'using' for typedefs

--- src/utils/timer.h
+++ src/utils/timer.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 John Mark White -- US Amateur Radio License: W4KUS
+//
+// Licensed under the MIT License - see LICENSE file for details.
+
 #pragma once
 
 #include <chrono>
@@ -8,7 +12,7 @@ class timer
 {
 public:
 
-    typedef std::chrono::steady_clock::time_point timer_t;
+    using timer_t = std::chrono::steady_clock::time_point;
 
     timer() = delete;
 
