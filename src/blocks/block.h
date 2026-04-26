1f37c4283c00356b18677d762b878bca7c3deac3
Mark White <w4kus.jmw@gmail.com>
2026-04-26 13:49:29 -0400

    Changes:
    * Use types from rm_math.
    * Add block type enumeration.
    * Add sampling rate.
    * Delete default constructor.

--- src/blocks/block.h
+++ src/blocks/block.h
@@ -8,6 +8,7 @@
 #include <type_traits>
 
 #include "aligned-ptr.h"
+#include "rm-math.h"
 
 namespace dsp {
 
@@ -19,37 +20,52 @@ struct is_block_func<void(const util::aligned_ptr<float>&, util::aligned_ptr<flo
         public std::true_type {};
 
 template<>
-struct is_block_func<void(const util::aligned_ptr<float>&, util::aligned_ptr<std::complex<float>>&)> :
+struct is_block_func<void(const util::aligned_ptr<float>&, util::aligned_ptr<rm_math::complex_f>&)> :
         public std::true_type {};
 
 template<>
-struct is_block_func<void(const util::aligned_ptr<std::complex<float>>&, util::aligned_ptr<std::complex<float>>&)> :
+struct is_block_func<void(const util::aligned_ptr<rm_math::complex_f>&, util::aligned_ptr<rm_math::complex_f>&)> :
         public std::true_type {};
 
 template<>
-struct is_block_func<void(const util::aligned_ptr<std::complex<float>>&, util::aligned_ptr<float>&)> :
+struct is_block_func<void(const util::aligned_ptr<rm_math::complex_f>&, util::aligned_ptr<float>&)> :
         public std::true_type {};
 
 template<typename T>
 constexpr bool is_block_func_v = is_block_func<T>::value;
 
 using func_ff = void(const util::aligned_ptr<float>&, util::aligned_ptr<float>&);
-using func_fc = void(const util::aligned_ptr<float>&, util::aligned_ptr<std::complex<float>>&);
-using func_cc = void(const util::aligned_ptr<std::complex<float>>&, util::aligned_ptr<std::complex<float>>&);
-using func_cf = void(const util::aligned_ptr<std::complex<float>>&, util::aligned_ptr<float>&);
+using func_fc = void(const util::aligned_ptr<float>&, util::aligned_ptr<rm_math::complex_f>&);
+using func_cc = void(const util::aligned_ptr<rm_math::complex_f>&, util::aligned_ptr<rm_math::complex_f>&);
+using func_cf = void(const util::aligned_ptr<rm_math::complex_f>&, util::aligned_ptr<float>&);
+
+using rate_t = uint32_t;
+
+enum block_type
+{
+    TYPE_OPERATOR,  // A block which performs some processing on the input samples and produces output samples.
+    TYPE_SOURCE,    // A block which generates input samples for the signal processing chain.
+    TYPE_SINK,      // A block which consumes output samples from the signal processing chain.
+    TYPE_BIDIR,     // A block which serves as both a source and sink in the signal processing chain.
+    TYPE_RESAMPLER  // An operator type block which resamples the signal.
+};
 
 template<typename T>
 class block
 {
 public:
-    auto getProcesser() { return process; }
+    auto        getProcesser() { return process; }
 
-    uint32_t getSamplingRate() { return m_SamplingRate; }
-    void  setSamplingRate(uint32_t rate) { m_SamplingRate = rate; }
+    void        setSamplingRate(uint32_t rate) { m_SamplingRate = rate; }
+    block_type  getType() { return m_Type; }
+
+    rate_t      getSamplingRate() { return m_SamplingRate; }
 
 protected:
-    block() : m_SamplingRate { 0 }
+    block(block_type type) : m_SamplingRate { 0 }, m_Type { type }
     { }
+
+    block() = delete;
 
     block(const block &) = delete;
     block& operator=(const block &) = delete;
@@ -59,6 +75,10 @@ protected:
 
     std::function<T> process;
 
-    uint32_t m_SamplingRate;
+    rate_t m_SamplingRate;
+
+private:
+    block_type  m_Type;
 };
+
 }
