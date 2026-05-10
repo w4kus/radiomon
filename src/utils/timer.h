02fbc87f2a1071c650e76a31f446c388721558c2
Mark White <w4kus.jmw@gmail.com>
2026-05-10 08:49:58 -0400

    Changes:
    * Redesign to allow different clock sources.
    * Add sleep methods.

--- src/utils/timer.h
+++ src/utils/timer.h
@@ -5,22 +5,25 @@
 #pragma once
 
 #include <chrono>
+#include <thread>
 
 namespace util {
-    
-class timer
-{
-public:
 
+// Time routines from the standard C++ library
+struct cpp
+{
     using timer_t = std::chrono::steady_clock::time_point;
 
-    timer() = delete;
+    cpp() = delete;
 
+    // Get the current time
     static timer_t StartTimer()
     {
         return std::chrono::steady_clock::now();
     }
 
+    // Return the elapsed time in milliseconds between these call and a previous call to
+    // StartTimer().
     static uint32_t EndTimer(timer_t &tmr)
     {
         auto now = std::chrono::steady_clock::now();
@@ -28,11 +31,26 @@ public:
         return std::chrono::duration_cast<std::chrono::milliseconds>(now - tmr).count();
     }
 
+    // Return the elapsed time in microseconds between these call and a previous call to
+    // StartTimer(). Some platform might not support microsecond resolution in which
+    // case this should do nothing.
     static uint32_t EndTimerUs(timer_t &tmr)
     {
         auto now = std::chrono::steady_clock::now();
 
         return std::chrono::duration_cast<std::chrono::microseconds>(now - tmr).count();
     }
+
+    static void sleep(uint32_t ms)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+    }
+
+    static void sleepUs(uint32_t us)
+    {
+        std::this_thread::sleep_for(std::chrono::microseconds(us));
+    }
 };
+
+using timer = cpp;
 }
