cfb7dec9e97461270e84d166d08c57e8d6f107dd
Mark White <w4kus.jmw@gmail.com>
2026-07-26 13:25:59 -0400

    Refactor to Simplify
    This block no longer supports user defined headers. It is now hard coded
    to an array of 5 bytes with one byte specifying the sample type and four
    bytes representing the sample rate.

--- src/blocks/zmq-sample-pub.h
+++ src/blocks/zmq-sample-pub.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <arpa/inet.h>
+
 #include "block.h"
 #include "zmq/sample-msg.h"
 
@@ -19,36 +21,33 @@ namespace dsp {
   * \tparam T            The type of *aligned_ptr* to create. Either *float* or *std::vector<float>*.
   * \tparam B            The block function type. See block.h.
   * \tparam block_type   The block type. See block.h.
-  * \tparam size         The size of a std::array if one is used for the header. Default is zero in which
-  *                      case a string is used for the header.
 */
 
-template<typename T, typename B, block_type type, uint8_t size = 0>
+union value_rate_t
+{
+    uint8_t bytes[4];
+    rate_t val;
+};
+
+constexpr uint8_t SAMPLES_FLOAT = 0x01;
+constexpr uint8_t SAMPLES_CMPLX = 0x02;
+
+template<typename T, typename B, block_type type>
 class zmq_sample_pub : public block<B>
 {
-    static_assert((std::is_floating_point<T>::value == std::true_type()) || util::is_std_complex_v<T>);
+    static_assert(std::is_floating_point<T>::value == std::true_type());
     static_assert((type == TYPE_OPERATOR) || (type == TYPE_SINK));
 
 public:
 
-    //! Create an instance using a C string as the ZMQ header
-    //! @param [in] socketId  The socket ID to use. The subscriber must use the same ID.
-    //! @param [in] hdr       A C string representing the header. The subscriber can use this for filtering.
-    zmq_sample_pub(const char *socketId, const char *hdr) : block<B> { type }, m_Msg { hdr }
+    //! Create an instance.
+    //! @param [in] socketId  The socket ID to use.
+    zmq_sample_pub(const char *socketId) : block<B> { type }
     {
+        m_Hdr = def_hdr;
+        m_Msg.header(m_Hdr);
         m_Msg.init(socketId);
-        block<B>::process = std::bind(&zmq_sample_pub::handler, this, std::placeholders::_1, std::placeholders::_2);
-    }
 
-    //! Create an instance using a byte array as the ZMQ header.
-    //! @param [in] socketId  The socket ID to use. The subscriber must use the same ID.
-    //! @param [in] hdr       A std::array representing the header. The subscriber can use this for filtering. The
-    //!                       size of the array is limited to 256 bytes.
-    zmq_sample_pub(const char *socketId, const std::array<uint8_t, size> &hdr) : block<B> { type }, m_Msg { hdr }
-    {
-        static_assert(size > 0);
-
-        m_Msg.init(socketId);
         block<B>::process = std::bind(&zmq_sample_pub::handler, this, std::placeholders::_1, std::placeholders::_2);
     }
 
@@ -58,7 +57,16 @@ public:
     //!                         Otherwise, it's ignored.
     void handler(const util::aligned_ptr<T> &in, util::aligned_ptr<T> &out)
     {
+        value_rate_t rate;
+        rate.val = htonl(block<B>::getSamplingRate());
+
         // Send to the socket
+        m_Hdr[1] = rate.bytes[0];
+        m_Hdr[2] = rate.bytes[1];
+        m_Hdr[3] = rate.bytes[2];
+        m_Hdr[4] = rate.bytes[3];
+
+        m_Msg.header(m_Hdr);
         m_Msg.send(in);
 
         // Copy in -> out
@@ -70,39 +78,75 @@ public:
     }
 
 private:
-    util::zmq::sample_msg<T, util::zmq::PUB_EP, size> m_Msg;
+    static constexpr std::array<uint8_t, 5> def_hdr   = { SAMPLES_FLOAT, 0x00, 0x00, 0x00, 0x00 };
+
+    util::zmq::sample_msg<T, util::zmq::PUB_EP, def_hdr.size()> m_Msg;
+    std::array<uint8_t, def_hdr.size()> m_Hdr;
 };
 
-// Convenient aliases for float->float operator and sink objects using string headers
+// Partial specialization for std::complex which requires a different header.
+template<block_type type>
+class zmq_sample_pub<rm_math::complex_f, func_cc, type> : public block<func_cc>
+{
+    static_assert((type == TYPE_OPERATOR) || (type == TYPE_SINK));
+
+public:
+
+    //! Create an instance.
+    //! @param [in] socketId  The socket ID to use. The subscriber must use the same ID.
+    zmq_sample_pub(const char *socketId) : block { type }
+    {
+        m_Hdr = def_hdr;
+        m_Msg.header(m_Hdr);
+        m_Msg.init(socketId);
+
+        process = std::bind(&zmq_sample_pub::handler, this, std::placeholders::_1, std::placeholders::_2);
+    }
+
+    //! Handle incoming samples.
+    //! @param [in]  inBlock    The block of input samples.
+    //! @param [out] outBlock   The block of processed samples. This only applies if the block type is TYPE_OPERATOR.
+    //!                         Otherwise, it's ignored.
+    void handler(const util::aligned_ptr<rm_math::complex_f> &in, util::aligned_ptr<rm_math::complex_f> &out)
+    {
+        value_rate_t rate;
+        rate.val = htonl(getSamplingRate());
+
+        // Send to the socket
+        m_Hdr[1] = rate.bytes[0];
+        m_Hdr[2] = rate.bytes[1];
+        m_Hdr[3] = rate.bytes[2];
+        m_Hdr[4] = rate.bytes[3];
+
+        m_Msg.header(m_Hdr);
+        m_Msg.send(in);
+
+        // Copy in -> out
+        if (getType() == TYPE_OPERATOR)
+        {
+            util::init_aligned_ptr_on_resize(out, in.size());
+            std::copy(in.begin(), in.end(), out.begin());
+        }
+    }
+
+private:
+    static constexpr std::array<uint8_t, 5> def_hdr   = { SAMPLES_CMPLX, 0x00, 0x00, 0x00, 0x00 };
+
+    util::zmq::sample_msg<rm_math::complex_f, util::zmq::PUB_EP, def_hdr.size()> m_Msg;
+    std::array<uint8_t, def_hdr.size()> m_Hdr;
+};
+
+
+// Convenient aliases for float->float operator and sink objects
 template<block_type T>
 using zmq_sample_pub_ff = zmq_sample_pub<float, func_ff, T>;
 using zmq_sample_pub_ff_op = zmq_sample_pub_ff<TYPE_OPERATOR>;
 using zmq_sample_pub_ff_snk = zmq_sample_pub_ff<TYPE_SINK>;
 
-// Convenient aliases for complex->complex operator and sink objects using string headers
+// Convenient aliases for complex->complex operator and sink objects
 template<block_type T>
 using zmq_sample_pub_cc = zmq_sample_pub<std::complex<float>, func_cc, T>;
 using zmq_sample_pub_cc_op = zmq_sample_pub_cc<TYPE_OPERATOR>;
 using zmq_sample_pub_cc_snk = zmq_sample_pub_cc<TYPE_SINK>;
-
-// Convenient aliases for float->float operator and sink objects using byte array headers
-template<block_type T, uint8_t size>
-using zmq_sample_pub_ff_array = zmq_sample_pub<float, func_ff, T, size>;
-
-template<uint8_t size>
-using zmq_sample_pub_ff_op_array = zmq_sample_pub<float, func_ff, TYPE_OPERATOR, size>;
-
-template<uint8_t size>
-using zmq_sample_pub_ff_snk_array = zmq_sample_pub<float, func_ff, TYPE_SINK, size>;
-
-// Convenient aliases for complex->complex operator and sink objects using byte array headers
-template<block_type T, uint8_t size>
-using zmq_sample_pub_cc_array = zmq_sample_pub<std::complex<float>, func_cc, T, size>;
-
-template<uint8_t size>
-using zmq_sample_pub_cc_op_array = zmq_sample_pub<std::complex<float>, func_cc, TYPE_OPERATOR, size>;
-
-template<uint8_t size>
-using zmq_sample_pub_cc_snk_array = zmq_sample_pub<std::complex<float>, func_cc, TYPE_SINK, size>;
 
 }
